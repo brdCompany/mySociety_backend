071f3de5841812862b96f012d80623c6b17b3162
bulbulalamgir <bulbul.alamgir@gmail.com>
2020-07-15 19:09:09 +0530

    initial backend code added for payments

--- routes/payments.js
+++ routes/payments.js
@@ -1,7 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
 // Base URL - /api/v1/payments/
 
 // / - GET - get all payments
+router.get('/', (req, res) => {
+  res.status(200).json({msg: "Get All payments"})
+});
 // / - POST - create payment
+router.post('/', (req, res) => {
+  res.status(200).json({msg: "Create payments"})
+});
 // /:id - GET - get single payment
+router.get('/:id', (req, res) => {
+  res.status(200).json({msg: "Get single payment"})
+});
 // /:id - PUT - update payment
+router.put('/:id', (req, res) => {
+  res.status(200).json({msg: "Update payment"})
+});
 // /:id - delete - delete payment
+router.delete('/:id', (req, res) => {
+  res.status(200).json({msg: "Delete payment"})
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
 //Routes
-const users = require('./routes/users');
-const bills = require('./routes/bills');
+const users = require("./routes/users");
+const bills = require("./routes/bills");
+const payments = require("./routes/payments");
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 const PORT = process.env.PORT || 5500;
 
 const app = express();
 
-app.use('/api/v1/users', users);
-app.use('/api/v1/bills', bills);
+app.use("/api/v1/users", users);
+app.use("/api/v1/bills", bills);
+app.use("/api/v1/payments", payments);
 
 app.listen(
   PORT,
