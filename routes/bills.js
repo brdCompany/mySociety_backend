13c9643213e5fff2c380bd5a3e409eb3c3252cd1
dishaganguly <disha.ganguly21@gmail.com>
2020-07-15 18:31:32 +0530

    Initial Backend code for Bills

--- routes/bills.js
+++ routes/bills.js
@@ -1,7 +1,27 @@
 // Base URL - /api/v1/bills/
+const express = require('express');
+const router = express('Router');
 
 // / - GET - get all bills
+router.get('/', (req, res) => {
+  res.status(200).json({ msg: 'Get all Bills' });
+});
 // / - POST - create bill
+router.post('/', (req, res) => {
+  res.status(200).json({ msg: 'Create Bill' });
+});
 // /:id - GET - get single bill
+
+router.get('/:id', (req, res) => {
+  res.status(200).json({ msg: 'Get single Bill' });
+});
 // /:id - PUT - update bill
+router.put('/:id', (req, res) => {
+  res.status(200).json({ msg: 'Update single Bill' });
+});
 // /:id - delete - delete bill
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ msg: 'Delete single Bill' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 
 //Routes
 const users = require('./routes/users');
+const bills = require('./routes/bills');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5500;
 const app = express();
 
 app.use('/api/v1/users', users);
+app.use('/api/v1/bills', bills);
 
 app.listen(
   PORT,
