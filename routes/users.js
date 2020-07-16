01854c0a0dbecf40433ac510e650d83057ca06d1
bulbulalamgir <bulbul.alamgir@gmail.com>
2020-07-16 23:30:47 +0530

    code added for update and delete user

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
 
 // api base - /api/v1/users
 
 // get all users - GET - /api/v1/users
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).send({ msg: 'Server Error' });
+    res.status(500).send({ msg: "Server Error" });
   }
 });
 
 // get single user GET - /api/v1/users/:email
-router.get('/:email', async (req, res) => {
+router.get("/:email", async (req, res) => {
   try {
     console.log(req.params.email);
     const user = await User.findOne({ email: req.params.email });
 
     res.status(200).send(user);
   } catch (error) {
-    res.status(500).json({ msg: 'Server Error' });
+    res.status(500).json({ msg: "Server Error" });
   }
-  res.status(200).json({ msg: 'Get single users' });
+  res.status(200).json({ msg: "Get single users" });
 });
 
 // create user - POST - /api/v1/users
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   User.create(req.body);
-  res.status(200).json({ msg: 'Create user' });
+  res.status(200).json({ msg: "Create user" });
 });
 
 // update user - PUT - /api/v1/users/:id
-router.put('/:id', (req, res) => {
-  res.status(200).json({ msg: 'Update user' });
+router.put("/:id", async (req, res) => {
+  try {
+    const user = new User({
+      _id: req.params.id,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      flat: req.body.flat,
+      block: req.body.block,
+    });
+    User.updateOne({ _id: req.params.id }, user).then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "User details successfully Updated !." });
+    });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
 
 // delele user - DELETE - /api/v1/users/:id
-router.delete('/:id', (req, res) => {
-  res.status(200).json({ msg: 'Delete user' });
+router.delete("/:email", async (req, res) => {
+  try {
+    User.deleteOne({ email: req.params.email }).then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "User successfully deleted from DB!" });
+    });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
 
 module.exports = router;
