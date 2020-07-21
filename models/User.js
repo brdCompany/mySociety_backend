0a78d798f7966b41e4ba516a9e407fa3a873e827
bulbulalamgir <bulbul.alamgir@gmail.com>
2020-07-21 10:47:50 +0530

    code changes of token generation and login

--- models/User.js
+++ models/User.js
@@ -1,26 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please provide a name'],
+    required: [true, "Please provide a name"],
   },
   email: {
     type: String,
-    required: [true, 'Please provide a email'],
+    required: [true, "Please provide a email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
   },
   phone: {
     type: String,
-    required: [true, 'Please provide a phone'],
+    required: [true, "Please provide a phone"],
   },
   flat: {
     type: String,
-    required: [true, 'Please provide a Flat No'],
+    required: [true, "Please provide a Flat No"],
   },
   block: {
     type: String,
-    required: [true, 'Please provide a Block No'],
+    required: [true, "Please provide a Block No"],
   },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jsonwebtoken = require("jsonwebtoken");
 
 // api base - /api/v1/users
 
 // get all users - GET - /api/v1/users
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).json({ msg: 'success', data: users });
+    res.status(200).json({ msg: "success", data: users });
   } catch (error) {
-    res.status(500).send({ msg: 'Server Error' });
+    res.status(500).send({ msg: "Server Error" });
   }
 });
 
 // get single user GET - /api/v1/users/:email
-router.get('/:email', async (req, res) => {
+router.get("/:email", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.params.email });
     // if (!user) {
     //   return res.status(400).json({ msg: 'Error getting User' });
     // }
-    res.status(200).json({ msg: 'success', data: user });
+    res.status(200).json({ msg: "success", data: user });
   } catch (error) {
     //res.status(500).json({ msg: 'Server Error' });
     next(error);
@@ -29,21 +31,61 @@ router.get('/:email', async (req, res) => {
 });
 
 // create user - POST - /api/v1/users
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    console.log(req.body.password);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
     const user = await User.create(req.body);
     if (!user) {
-      return res.status(400).json({ msg: 'Error creating User' });
+      return res.status(400).json({ msg: "Error creating User" });
     }
-    res.status(200).json({ msg: 'Success', data: user });
+    res.status(200).json({ msg: "Success", data: user });
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ msg: 'Server Error' });
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+router.post("/login", (req, res) => {
+  try {
+    let fetchedUser;
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          return res.status(401).json({
+            message: "User Authentication failed",
+          });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+      })
+      .then((result) => {
+        if (!result) {
+          console.log(result);
+          return res.status(401).json({
+            message: "User Authentication failed",
+          });
+        }
+        const token = jsonwebtoken.sign(
+          { email: fetchedUser.email },
+          process.env.TOKEN_SECRET_ID,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({
+          token: token,
+          expiresIn: 3600,
+        });
+      });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Server Error",
+    });
   }
 });
 
 // update user - PUT - /api/v1/users/:id
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const user = new User({
       _id: req.params.id,
@@ -55,22 +97,22 @@ router.put('/:id', async (req, res) => {
     });
     User.updateOne({ _id: req.params.id }, user).then((result) => {
       console.log(result);
-      res.status(200).json({ message: 'User details successfully Updated !.' });
+      res.status(200).json({ message: "User details successfully Updated !." });
     });
   } catch (error) {
-    res.status(500).json({ msg: 'Server Error' });
+    res.status(500).json({ msg: "Server Error" });
   }
 });
 
 // delele user - DELETE - /api/v1/users/:id
-router.delete('/:email', async (req, res) => {
+router.delete("/:email", async (req, res) => {
   try {
     User.deleteOne({ email: req.params.email }).then((result) => {
       console.log(result);
-      res.status(200).json({ message: 'User successfully deleted from DB!' });
+      res.status(200).json({ message: "User successfully deleted from DB!" });
     });
   } catch (error) {
-    res.status(500).json({ msg: 'Server Error' });
+    res.status(500).json({ msg: "Server Error" });
   }
 });
 
